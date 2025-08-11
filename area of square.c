2205f9a0b5f794ece01899f07c4f2eb32cbbb977
preetdonga <preetdonga07@gmail.com>
2025-08-11 16:56:38 +0530

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    int a,l,p;
+    printf("enter value of side :");
+    scanf("%d",&l);
+    a=l*l;
+    p=4*l;
+    printf("Area of the square: %d\n", a);
+    printf("Perimeter of the square: %d\n", p);
+}
