4c3252c2f951f83324dc417e8970c361911a074a
preetdonga <preetdonga07@gmail.com>
2025-08-11 22:23:45 +0530

    Add files via upload

---
+++ average of 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+
+   {
+float a,b,c,d,t;
+    printf("enter value for first subject :");
+    scanf("%f",&a);
+    printf("enter value for second subject :");
+    scanf("%f",&b);
+    printf("enter value for third subject :");
+    scanf("%f",&c);
+    d=(a+b+c)/3;
+    t=a+b+c;
+    printf("your average is :%f",d);
+    printf("\ntotal is :%f",t);
+    }
