5b879e628bca20276aed18c7710e22b10bb83a32
preetdonga <preetdonga07@gmail.com>
2025-08-11 22:04:56 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+ {
+    float a,h,l;
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &l);
+    printf("Enter the high of the rectangle: ");
+    scanf("%f", &h);
+    a=(h*l)/2;
+    printf("Area of the tirangle = %f", a);
+}
